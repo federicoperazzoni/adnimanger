6199f8f2b6e8f368cee1f2da483dd0a9f2a99dd2
Federico Perazzoni <f.perazzoni@students.uninettunouniversity.net>
2019-07-14 10:07:10 +0200

    Neuro battery

--- Jena/src/main/java/jena/adni/bean/NeuroBatteryBean.java
+++ Jena/src/main/java/jena/adni/bean/NeuroBatteryBean.java
@@ -21,4 +21,68 @@ public class NeuroBatteryBean extends ADNIBean {
 	//errors (0-17) (71) ADNI1 ADNIGO ADNI2 ADNI3
 	//AVDELERR2
 	private String Avdel_Err2;
+	
+	//yes/no recognition trial of 50 words included List A, B and other words (0-15) (70) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVDELTOT
+	private String Avdel_Tot;
+	
+	// N.A. (39) ADNI2 ADNI3
+	//AVENDED
+	private String Avended;
+	
+	//Intrusions in immediate recall of List A (0-15) (26) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVERR1
+	private String Averr1;
+	
+	//Intrusions in immediate recall of List A (0-15) (28) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVERR2
+	private String Averr2;
+	
+	//Intrusions in immediate recall of List A (0-15) (30) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVERR3
+	private String Averr3;
+	
+	//Intrusions in immediate recall of List A (0-15) (32) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVERR4
+	private String Averr4;
+	
+	//Intrusions in immediate recall of List A (0-15) (34) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVERR5
+	private String Averr5;
+	
+	//Intrusions in immediate recall of List A (0-15) (36) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVERR6
+	private String Averr6;
+	
+	//Intrusions in immediate recall of List B (0-15) (38) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVERRB
+	private String AverrB;
+	
+	//Number of words in immediate recall of List A (0-15) (25) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVTOT1
+	private String Avtot1;
+	
+	//Number of words in immediate recall of List A (0-15) (27) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVTOT2
+	private String Avtot2;
+	
+	//Number of words in immediate recall of List A (0-15) (29) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVTOT3
+	private String Avtot3;
+	
+	//Number of words in immediate recall of List A (0-15) (31) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVTOT4
+	private String Avtot4;
+	
+	//Number of words in immediate recall of List A (0-15) (33) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVTOT5
+	private String Avtot5;
+	
+	//Number of words in immediate recall of List A (0-15) (35) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVTOT6
+	private String Avtot6;
+	
+	//Number of words in immediate recall of List B (0-15) (37) ADNI1 ADNIGO ADNI2 ADNI3
+	//AVTOTB
+	private String AvtotB;
 }
