f89bf06d384c98c918e78ed09476215021f702dc
Federico Perazzoni <f.perazzoni@students.uninettunouniversity.net>
2019-07-21 12:09:45 +0200

    Neuro battery

---
+++ Jena/src/main/java/jena/adni/loader/LoaderNeuroBatteryCsvToBeanArray.java
@@ -0,0 +1,67 @@
+package jena.adni.loader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import jena.adni.bean.CDRBean;
+import jena.adni.bean.NeuroBatteryBean;
+import jena.adni.bean.SubjectDataBean;
+
+public class LoaderNeuroBatteryCsvToBeanArray {
+
+	public ArrayList<NeuroBatteryBean> load(String pathCsv) {
+
+		ArrayList<NeuroBatteryBean> neuroBatteryTestList = new ArrayList<NeuroBatteryBean>();
+
+		Path myPath = Paths.get(pathCsv);
+
+		CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+
+		try { BufferedReader br = Files.newBufferedReader(myPath, 
+				StandardCharsets.UTF_8);
+		CSVReader reader = new CSVReaderBuilder(br).withCSVParser(parser)
+				.build();
+
+		List<String[]> rows = reader.readAll();
+
+		int count = 0;
+
+		for (String[] row : rows) {
+
+			if (count > 0) {
+
+				NeuroBatteryBean neuroBatteryBean = new NeuroBatteryBean();
+
+				//Vanno le info
+				
+				neuroBatteryBean.setSubjectDataBean(new SubjectDataBean());
+				
+				neuroBatteryBean.getSubjectDataBean().setRID(row[2].replaceAll("\"", ""));
+
+				neuroBatteryTestList.add(neuroBatteryBean);
+			}
+			
+			count++;
+		}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		return neuroBatteryTestList;
+	}
+}
