dfd3ea48db05ae321de1896a1b8db6b8ae43b9b2
Federico Perazzoni <f.perazzoni@students.uninettunouniversity.net>
2019-07-30 23:39:42 +0200

    NPI

---
+++ Jena/src/main/java/jena/adni/bean/NPIBean.java
@@ -0,0 +1,18 @@
+package jena.adni.bean;
+
+public class NPIBean extends ADNIBean {
+
+	//Does the patient have beliefs that you know are not true (for example, insisting that people are trying to harm him/her or steal from him/her)? 
+	//Has he/she said that family members are not who they say they are or that the house is not their home? I’m not asking about mere suspiciousness; 
+	//I am interested if the patient is convinced that these things are happening to him/her. (Yes/No) (12) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA
+	private String Npia;
+	
+	//Does the patient believe that he/she is in danger - that others are planning to hurt him/her? (Yes/No) (13) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA1
+	private String Npia1;
+	
+	//Frequency of the symptom (1: Occasionally; 2: Often; 3: Frequently; 4: Very frequently) (22) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA10A
+	private String Npia10a;
+}
