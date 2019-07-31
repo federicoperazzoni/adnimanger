54ddbde69ea939e25eb4fddc0cd7df2ee9a5943f
Federico Perazzoni <f.perazzoni@students.uninettunouniversity.net>
2019-07-31 07:10:50 +0200

    NPI

--- Jena/src/main/java/jena/adni/bean/NPIBean.java
+++ Jena/src/main/java/jena/adni/bean/NPIBean.java
@@ -15,4 +15,48 @@ public class NPIBean extends ADNIBean {
 	//Frequency of the symptom (1: Occasionally; 2: Often; 3: Frequently; 4: Very frequently) (22) ADNI1 ADNIGO ADNI2 ADNI3
 	//NPIA10A
 	private String Npia10a;
+	
+	//Severity of the symptom (1: Mild; 2: Moderate; 3: Marked) (23) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA10B
+	private String Npia10b;
+	
+	//DISTRESS experienced by Caregiver due to the symptom  (0 :Not distressing at all;  1: Minimal; 2:Mild; 3 : Moderate; 4 : Severe 5 :Extreme or Very Severe) (24) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA10C
+	private String Npia10c;
+	
+	//Does the patient believe that others are stealing from him/her? (Yes/No) (14) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA2
+	private String Npia2;
+	
+	//Does the patient believe that his/her spouse is having an affair? (Yes/No) (15) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA3
+	private String Npia3;
+	
+	//Does the patient believe that unwelcome guests are living in his/her house? (Yes/No) (16) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA4
+	private String Npia4;
+	
+	//Does the patient believe that his/her spouse or others are not who they claim to be? (Yes/No) (17) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA5
+	private String Npia5;
+	
+	//Does the patient believe that his/her house is not his/her home? (Yes/No) (18) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA6
+	private String Npia6;
+	
+	//Does the patient believe that family members plan to abandon him/her? (Yes/No) (19) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA7
+	private String Npia7;
+	
+	//Does the patient believe that television or magazine figures are actually present in the home?  [Does he/she try to talk to or interact with them?] (Yes/No) (20) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA8
+	private String Npia8;
+	
+	//Does the patient believe any other unusual things that I haven't asked about? (Yes/No) (21) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIA9
+	private String Npia9;
+	
+	//Frequency x Severity (0-12) (25) ADNI1 ADNIGO ADNI2 ADNI3
+	//NPIATOT
+	private String Npiatot;
 }
