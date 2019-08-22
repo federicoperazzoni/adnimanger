c92f8feaa758ea5e02e1557d9db4f6e5f2c8916f
Federico Perazzoni <fperazzoni@gmail.com>
2019-08-22 16:02:36 +0200

    GDS e NPIQBean

---
+++ Jena/src/main/java/jena/adni/bean/GDSBean.java
@@ -0,0 +1,40 @@
+package jena.adni.bean;
+
+public class GDSBean extends ADNIBean {
+
+	//()
+	//(Yes / No)
+	//Are you afraid that something bad is going to happen to you?
+	//GDAFRAID
+	private String Gdafraid;
+	
+	//()
+	//(Yes / No)
+	//Do you think its wonderful to be alive now? 
+	//GDALIVE
+	private String Gdalive;
+	
+	//()
+	//(Yes / No)
+	//Do you think that most people are better off than you are?
+	//GDBETTER
+	private String Gdbetter;
+	
+	//()
+	//(Yes / No)
+	//Do you often get bored?
+	//GDBORED
+	private String Gdbored;
+	
+	//()
+	//(Yes / No)
+	//Have you dropped many of your activities and interests? 
+	//GDDROP
+	private String Gddrop;
+	
+	//()
+	//(Yes / No)
+	//Do you feel that your life is empty?
+	//GDEMPTY
+	private String Gdempty;
+}
---
+++ Jena/src/main/java/jena/adni/bean/NPIQBean.java
@@ -0,0 +1,302 @@
+package jena.adni.bean;
+
+public class NPIQBean extends ADNIBean {
+
+	//NPIA
+	private String Npia;
+	
+	//NPIASEV
+	private String Npiasev;
+	
+	//NPIB
+	private String Npib;
+	
+	//NPIBSEV
+	private String Npibsev;
+	
+	//NPIC
+	private String Npic;
+	
+	//NPICSEV
+	private String Npicsev;
+	
+	//NPID
+	private String Npid;
+	
+	//NPIDSEV
+	private String Npidsev;
+	
+	//NPIE
+	private String Npie;
+	
+	//NPIESEV
+	private String Npiesev;
+	
+	//NPIF
+	private String Npif;
+	
+	//NPIFSEV
+	private String Npifsev;
+	
+	//NPIG
+	private String Npig;
+	
+	//NPIGSEV
+	private String Npigsev;
+	
+	//NPIH
+	private String Npih;
+	
+	//NPIHSEV
+	private String Npihsev;
+	
+	//NPII
+	private String Npii;
+	
+	//NPIISEV
+	private String Npiisev;
+	
+	//NPIJ
+	private String Npij;
+	
+	//NPIJSEV
+	private String Npijsev;
+	
+	//NPIK
+	private String Npik;
+	
+	//NPIKSEV
+	private String Npiksev;
+	
+	//NPIL
+	private String Npil;
+	
+	//NPILSEV
+	private String Npilsev;
+	
+	//EXAMDATE
+	private String Examdate;
+	
+	//SITEID
+	private String Siteid;
+	
+	//VISCODE
+	private String Viscode;
+
+	public String getNpia() {
+		return Npia;
+	}
+
+	public void setNpia(String npia) {
+		Npia = npia;
+	}
+
+	public String getNpiasev() {
+		return Npiasev;
+	}
+
+	public void setNpiasev(String npiasev) {
+		Npiasev = npiasev;
+	}
+
+	public String getNpib() {
+		return Npib;
+	}
+
+	public void setNpib(String npib) {
+		Npib = npib;
+	}
+
+	public String getNpibsev() {
+		return Npibsev;
+	}
+
+	public void setNpibsev(String npibsev) {
+		Npibsev = npibsev;
+	}
+
+	public String getNpic() {
+		return Npic;
+	}
+
+	public void setNpic(String npic) {
+		Npic = npic;
+	}
+
+	public String getNpicsev() {
+		return Npicsev;
+	}
+
+	public void setNpicsev(String npicsev) {
+		Npicsev = npicsev;
+	}
+
+	public String getNpid() {
+		return Npid;
+	}
+
+	public void setNpid(String npid) {
+		Npid = npid;
+	}
+
+	public String getNpidsev() {
+		return Npidsev;
+	}
+
+	public void setNpidsev(String npidsev) {
+		Npidsev = npidsev;
+	}
+
+	public String getNpie() {
+		return Npie;
+	}
+
+	public void setNpie(String npie) {
+		Npie = npie;
+	}
+
+	public String getNpiesev() {
+		return Npiesev;
+	}
+
+	public void setNpiesev(String npiesev) {
+		Npiesev = npiesev;
+	}
+
+	public String getNpif() {
+		return Npif;
+	}
+
+	public void setNpif(String npif) {
+		Npif = npif;
+	}
+
+	public String getNpifsev() {
+		return Npifsev;
+	}
+
+	public void setNpifsev(String npifsev) {
+		Npifsev = npifsev;
+	}
+
+	public String getNpig() {
+		return Npig;
+	}
+
+	public void setNpig(String npig) {
+		Npig = npig;
+	}
+
+	public String getNpigsev() {
+		return Npigsev;
+	}
+
+	public void setNpigsev(String npigsev) {
+		Npigsev = npigsev;
+	}
+
+	public String getNpih() {
+		return Npih;
+	}
+
+	public void setNpih(String npih) {
+		Npih = npih;
+	}
+
+	public String getNpihsev() {
+		return Npihsev;
+	}
+
+	public void setNpihsev(String npihsev) {
+		Npihsev = npihsev;
+	}
+
+	public String getNpii() {
+		return Npii;
+	}
+
+	public void setNpii(String npii) {
+		Npii = npii;
+	}
+
+	public String getNpiisev() {
+		return Npiisev;
+	}
+
+	public void setNpiisev(String npiisev) {
+		Npiisev = npiisev;
+	}
+
+	public String getNpij() {
+		return Npij;
+	}
+
+	public void setNpij(String npij) {
+		Npij = npij;
+	}
+
+	public String getNpijsev() {
+		return Npijsev;
+	}
+
+	public void setNpijsev(String npijsev) {
+		Npijsev = npijsev;
+	}
+
+	public String getNpik() {
+		return Npik;
+	}
+
+	public void setNpik(String npik) {
+		Npik = npik;
+	}
+
+	public String getNpiksev() {
+		return Npiksev;
+	}
+
+	public void setNpiksev(String npiksev) {
+		Npiksev = npiksev;
+	}
+
+	public String getNpil() {
+		return Npil;
+	}
+
+	public void setNpil(String npil) {
+		Npil = npil;
+	}
+
+	public String getNpilsev() {
+		return Npilsev;
+	}
+
+	public void setNpilsev(String npilsev) {
+		Npilsev = npilsev;
+	}
+
+	public String getExamdate() {
+		return Examdate;
+	}
+
+	public void setExamdate(String examdate) {
+		Examdate = examdate;
+	}
+
+	public String getSiteid() {
+		return Siteid;
+	}
+
+	public void setSiteid(String siteid) {
+		Siteid = siteid;
+	}
+
+	public String getViscode() {
+		return Viscode;
+	}
+
+	public void setViscode(String viscode) {
+		Viscode = viscode;
+	}
+;
+}
