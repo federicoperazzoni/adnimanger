a444721b6989071f862f3f872ff835867c949211
Federico Perazzoni <fperazzoni@gmail.com>
2019-08-23 11:22:15 +0200

    GDS e NPIQBean

--- Jena/src/main/java/jena/adni/bean/GDSBean.java
+++ Jena/src/main/java/jena/adni/bean/GDSBean.java
@@ -37,4 +37,290 @@ public class GDSBean extends ADNIBean {
 	//Do you feel that your life is empty?
 	//GDEMPTY
 	private String Gdempty;
+	
+	//()
+	//(Yes / No)
+	//Do you feel full of energy?
+	//GDENERGY
+	private String Gdenergy;
+	
+	//()
+	//(Yes / No)
+	//Do you feel happy most of the time?
+	//GDHAPPY
+	private String Gdhappy;
+	
+	//()
+	//(Yes / No)
+	//Do you often feel helpless?
+	//GDHELP
+	private String Gdhelp;
+	
+	//()
+	//(Yes / No)
+	//Do you prefer to stay at home, rather than going out and doing new things?
+	//GDHOME
+	private String Gdhome;
+	
+	//()
+	//(Yes / No)
+	//Do you feel that your situation is hopeless?
+	//GDHOPE
+	private String Gdhope;
+	
+	//()
+	//(Yes / No)
+	//Do you feel you have more problems with memory than most?
+	//GDMEMORY
+	private String Gdmemory;
+	
+	//()
+	//(Yes / No)
+	//Are you basically satisfied with your life?
+	//GDSATIS
+	private String Gdsatis;
+	
+	//()
+	//(Yes / No)
+	//Are you in good spirits most of the time?
+	//GDSPIRIT
+	private String Gdspirit;
+	
+	//()
+	//(0 - 14)
+	//Total score
+	//GDTOTAL
+	private String Gdtotal;
+	
+	//()
+	//(Box checked ;  Box not checked ;)
+	//Participant is unable to complete the GDS based on the clinician's best judgement. 
+	//GDUNABL
+	private String Gdunabl;
+	
+	//()
+	//(Written response)
+	//If unable, explain
+	//GDUNABSP
+	private String Gdunabsp;
+	
+	//()
+	//(Yes / No)
+	//Do you feel pretty worthless the way you are now?
+	//GDWORTH
+	private String Gdworth;
+	
+	//()
+	//(Participant visit; Telephone call)
+	//Participant visit or Telephone call
+	//SOURCE
+	private String Source;
+	
+	//()
+	//(date)
+	//Exam date
+	//EXAMDATE
+	private String Examdate;
+	
+	//()
+	//(Participant visit; Telephone call)
+	//Participant visit or Telephone call
+	//GDSOURCE
+	private String Gdsource;
+	
+	//()
+	//(N.A.)
+	//N.A.
+	//ORIGPROT
+	private String Origprot;
+	
+	//()
+	//(Visit code)
+	//Visit code
+	//VISCODE
+	private String Viscode;
+
+	public String getGdafraid() {
+		return Gdafraid;
+	}
+
+	public void setGdafraid(String gdafraid) {
+		Gdafraid = gdafraid;
+	}
+
+	public String getGdalive() {
+		return Gdalive;
+	}
+
+	public void setGdalive(String gdalive) {
+		Gdalive = gdalive;
+	}
+
+	public String getGdbetter() {
+		return Gdbetter;
+	}
+
+	public void setGdbetter(String gdbetter) {
+		Gdbetter = gdbetter;
+	}
+
+	public String getGdbored() {
+		return Gdbored;
+	}
+
+	public void setGdbored(String gdbored) {
+		Gdbored = gdbored;
+	}
+
+	public String getGddrop() {
+		return Gddrop;
+	}
+
+	public void setGddrop(String gddrop) {
+		Gddrop = gddrop;
+	}
+
+	public String getGdempty() {
+		return Gdempty;
+	}
+
+	public void setGdempty(String gdempty) {
+		Gdempty = gdempty;
+	}
+
+	public String getGdenergy() {
+		return Gdenergy;
+	}
+
+	public void setGdenergy(String gdenergy) {
+		Gdenergy = gdenergy;
+	}
+
+	public String getGdhappy() {
+		return Gdhappy;
+	}
+
+	public void setGdhappy(String gdhappy) {
+		Gdhappy = gdhappy;
+	}
+
+	public String getGdhelp() {
+		return Gdhelp;
+	}
+
+	public void setGdhelp(String gdhelp) {
+		Gdhelp = gdhelp;
+	}
+
+	public String getGdhome() {
+		return Gdhome;
+	}
+
+	public void setGdhome(String gdhome) {
+		Gdhome = gdhome;
+	}
+
+	public String getGdhope() {
+		return Gdhope;
+	}
+
+	public void setGdhope(String gdhope) {
+		Gdhope = gdhope;
+	}
+
+	public String getGdmemory() {
+		return Gdmemory;
+	}
+
+	public void setGdmemory(String gdmemory) {
+		Gdmemory = gdmemory;
+	}
+
+	public String getGdsatis() {
+		return Gdsatis;
+	}
+
+	public void setGdsatis(String gdsatis) {
+		Gdsatis = gdsatis;
+	}
+
+	public String getGdspirit() {
+		return Gdspirit;
+	}
+
+	public void setGdspirit(String gdspirit) {
+		Gdspirit = gdspirit;
+	}
+
+	public String getGdtotal() {
+		return Gdtotal;
+	}
+
+	public void setGdtotal(String gdtotal) {
+		Gdtotal = gdtotal;
+	}
+
+	public String getGdunabl() {
+		return Gdunabl;
+	}
+
+	public void setGdunabl(String gdunabl) {
+		Gdunabl = gdunabl;
+	}
+
+	public String getGdunabsp() {
+		return Gdunabsp;
+	}
+
+	public void setGdunabsp(String gdunabsp) {
+		Gdunabsp = gdunabsp;
+	}
+
+	public String getGdworth() {
+		return Gdworth;
+	}
+
+	public void setGdworth(String gdworth) {
+		Gdworth = gdworth;
+	}
+
+	public String getSource() {
+		return Source;
+	}
+
+	public void setSource(String source) {
+		Source = source;
+	}
+
+	public String getExamdate() {
+		return Examdate;
+	}
+
+	public void setExamdate(String examdate) {
+		Examdate = examdate;
+	}
+
+	public String getGdsource() {
+		return Gdsource;
+	}
+
+	public void setGdsource(String gdsource) {
+		Gdsource = gdsource;
+	}
+
+	public String getOrigprot() {
+		return Origprot;
+	}
+
+	public void setOrigprot(String origprot) {
+		Origprot = origprot;
+	}
+
+	public String getViscode() {
+		return Viscode;
+	}
+
+	public void setViscode(String viscode) {
+		Viscode = viscode;
+	}
 }
